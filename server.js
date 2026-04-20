39eec773db35066e4591c6b68c4e1a9730d7e93f
teejey ahmad <ahmadtj111@gmail.com>
2026-04-20 12:42:27 +0100

    for vercel

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.get(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
